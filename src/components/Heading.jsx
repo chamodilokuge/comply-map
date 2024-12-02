722a58aac413c891eb1e53e1f1913cdfe241e3a2
chamlokuge <chamodi.16@itfac.mrt.ac.lk>
2024-12-02 21:10:11 +0000

    feat(refactor) - modularise the components

---
+++ src/components/Heading.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Heading = () => (
+  <header>
+    <h1>Nearby Restaurants and Cafes</h1>
+    <p>Explore restaurants and cafes around Comply Cube HQ!</p>
+  </header>
+);
+
+export default Heading;
